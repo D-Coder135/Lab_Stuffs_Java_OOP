f9af56152f8eb25d0eceea8617971923ef673703
Devansh Shukla <devansh.shukla_cs19@gla.ac.in>
2020-10-27 00:07:55 +0530

    add number of days for each month stored in the enum

--- labAssignment6/Months.java
+++ labAssignment6/Months.java
@@ -1,7 +1,18 @@
 package labAssignment6;
 
 public enum Months {
-    JANUARY, FEBRUARY, MARCH, APRIL, MAY, JUNE, JULY, AUGUST, SEPTEMBER, OCTOBER, NOVEMBER, DECEMBER;
+    JANUARY(31),
+    FEBRUARY(28),
+    MARCH(31),
+    APRIL(30),
+    MAY(31),
+    JUNE(30),
+    JULY(31),
+    AUGUST(31),
+    SEPTEMBER(30),
+    OCTOBER(31),
+    NOVEMBER(30),
+    DECEMBER(31);
     private final int numberOfDays;
 
     Months(int numberOfDays) {
