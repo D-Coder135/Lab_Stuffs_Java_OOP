e40b3ef9b5907ba7f9877025fd3348bde75902c7
Devansh Shukla <devansh.shukla_cs19@gla.ac.in>
2020-10-26 23:32:14 +0530

    add files to package labAssignment1

---
+++ labAssignment1/Task2.java
@@ -0,0 +1,30 @@
+/* Created By IntelliJ IDEA
+ *  Author: Devansh Shukla (191500255))
+ *  Date: 20-07-2020
+ *  Time: 01:18 PM
+ */
+package labAssignment1;
+
+//Task 2 - Create a program in Java to declare, initialize and use local variables of all the primitive data types in Java.
+public class Task2 {
+    public static void main(String[] args) {
+        byte byteVariable = 100;
+        short shortVariable = 2300;
+        char characterVariable = 'A';
+        int integerVariable = 125465;
+        long longVariable = 124569876;
+        float floatVariable = 5.26f;
+        double doubleVariable = 5.23546d;
+        boolean booleanVariable = true;
+        System.out.println("Byte Data Type = " + byteVariable);
+        System.out.println("Short Data Type = " + shortVariable);
+        System.out.println("Char Data Type = " + characterVariable);
+        System.out.println("Int Data Type = " + integerVariable);
+        System.out.println("Long Data Type = " + longVariable);
+        System.out.println("Float Data Type = " + floatVariable);
+        System.out.println("Double Data Type = " + doubleVariable);
+        System.out.println("Boolean Data Type = " + booleanVariable);
+
+
+    }
+}
