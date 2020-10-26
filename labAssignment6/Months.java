ddd0a8a41701c3afb2bfeecc4204c3d373cd2388
Devansh Shukla <devansh.shukla_cs19@gla.ac.in>
2020-10-27 00:04:26 +0530

    Add a parametrized constructor with numberOfDays as the parameter

--- labAssignment6/Months.java
+++ labAssignment6/Months.java
@@ -3,5 +3,9 @@ package labAssignment6;
 public enum Months {
     JANUARY, FEBRUARY, MARCH, APRIL, MAY, JUNE, JULY, AUGUST, SEPTEMBER, OCTOBER, NOVEMBER, DECEMBER;
     private final int numberOfDays;
-    
+
+    Months(int numberOfDays) {
+        this.numberOfDays = numberOfDays;
+    }
+
 }
