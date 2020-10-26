2226835209d90de3c60a7137ae64457d26dba7a7
Devansh Shukla <devansh.shukla_cs19@gla.ac.in>
2020-10-27 00:03:26 +0530

    Add a private final field name numberOfDays

--- labAssignment6/Months.java
+++ labAssignment6/Months.java
@@ -2,4 +2,6 @@ package labAssignment6;
 
 public enum Months {
     JANUARY, FEBRUARY, MARCH, APRIL, MAY, JUNE, JULY, AUGUST, SEPTEMBER, OCTOBER, NOVEMBER, DECEMBER;
+    private final int numberOfDays;
+    
 }
