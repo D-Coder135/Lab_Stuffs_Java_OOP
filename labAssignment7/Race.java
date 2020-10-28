5d1b88435006890c6893739d55c5a8fb6dd3a6e1
Devansh Shukla <devansh.shukla_cs19@gla.ac.in>
2020-10-28 19:37:54 +0530

    Create a execution class Race.java. Completed the Assignment! Final Commit!

---
+++ labAssignment7/Race.java
@@ -0,0 +1,10 @@
+package labAssignment7;
+
+public class Race {
+    public static void main(String[] args) {
+        Rabbit rabbit = new Rabbit();
+        rabbit.getRabbit().start();
+        Tortoise tortoise = new Tortoise();
+        tortoise.start();
+    }
+}
